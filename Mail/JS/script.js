ad135b4bebf118a02c197ee2332142aa60e3b02c
AndVig <andrea.vigna2@gmail.com>
2024-05-02 18:12:53 +0200

    Fine prima parte (MAIL)

--- Mail/JS/script.js
+++ Mail/JS/script.js
@@ -2,19 +2,16 @@ const emails = ["renato@mail.it" , "carlo@mail.com", "micuzzo@mail.com"];
 const btn = document.getElementById("btn");
 const mail = document.getElementById("control");
 const accesso = document.getElementById ("login");
-const prova = document.querySelector('.access_button');
-for (let i = 0 ; i < emails.length; i++ ){
-    if(mail.value == emails[0]){
-        accesso.innerText == ("Bentornato Renè");
-        prova.style.display = 'block';
-    }else if(mail.value == emails[1]){
-        accesso.innerText = ("Bentornato Carlo");
-        prova.classList.remove('access_button')
-    }else if(mail.value == emails[2]){
-        accesso.innerText = ("Bentornato Michè");
-        prova.classList.remove('access_button')
-    }else{
-        accesso.innerText = ("I dati inseriti non sono validi");
-
+const prova = document.querySelector('.access_button')
+btn.addEventListener('click', function () {
+    for (let i = 0 ; i < emails.length; i++ ){
+        if(mail.value == emails[i]){
+    
+            accesso.innerText = ("Bentornato");
+            prova.style.display = 'block';
+        }else{
+            accesso.innerText = ("I dati inseriti non sono validi");
+    
+        }
     }
-}
+})
