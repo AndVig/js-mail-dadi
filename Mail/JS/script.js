772a013c3edcf4b65c8cd9be14e42468d6e9920e
AndVig <andrea.vigna2@gmail.com>
2024-05-04 00:22:50 +0200

    aggiunto funzione che aggiunge un'email alla lista di quelle permesse

--- Mail/JS/script.js
+++ Mail/JS/script.js
@@ -1,17 +1,22 @@
 const emails = ["renato@mail.it" , "carlo@mail.com", "micuzzo@mail.com"];
-const btn = document.getElementById("btn");
+const btnLog = document.getElementById("btnLog");
+const btnAdd = document.getElementById("btnAdd");
 const mail = document.getElementById("control");
 const accesso = document.getElementById ("login");
 const prova = document.querySelector('.access_button')
-btn.addEventListener('click', function () {
+btnLog.addEventListener('click', function () {
     for (let i = 0 ; i < emails.length; i++ ){
         if(mail.value == emails[i]){
     
             accesso.innerText = ("Bentornato");
-            prova.style.display = 'block';
+            prova.style.display = 'inline';
         }else{
             accesso.innerText = ("I dati inseriti non sono validi");
     
         }
     }
+})
+btnAdd.addEventListener('click',function(){
+    emails.push(mail.value);
+    accesso.innerText=(mail.value +" correttamente aggiunto alla lista");
 })
